d4caff5997f8b445705618eefb3df68a3568ed2f
VolodymyrDvizhona <mlunch12@gmail.com>
2024-06-05 22:41:48 +0200

    Add project files.

---
+++ Program/Program.cs
@@ -0,0 +1,123 @@
+﻿using BazaZplytami;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Program
+{
+    class Program
+    {
+        static List<Baza.Plyta> bazaPlyt = new List<Baza.Plyta>();
+
+        static void Main(string[] args)
+        {
+            char c;
+            do
+            {
+                c = Menu();
+                switch (c)
+                {
+                    case 'a':
+                    case 'A':
+                        DodajPlyte();
+                        break;
+                    case 'b':
+                    case 'B':
+                        WypiszTytulyPlyt();
+                        break;
+                    case 'c':
+                    case 'C':
+                        WyswietlSzczegolyPlyty();
+                        break;
+                    case 'd':
+                    case 'D':
+                        ZapiszBazeDoPliku();
+                        break;
+                }
+            }
+            while (c != 'k' && c != 'K');
+        }
+
+        static char Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\t\tA - Dodaj płytę");
+            Console.WriteLine("\n\t\tB - Wypisz wszystkie tytuły płyt");
+            Console.WriteLine("\n\t\tC - Wyświetl szczegóły płyty");
+            Console.WriteLine("\n\t\tD - Zapisz bazę do pliku");
+            Console.WriteLine("\n\t\tK - Zakończ program");
+            return Console.ReadKey().KeyChar;
+        }
+
+        static void DodajPlyte()
+        {
+            Baza.Plyta plyta = new Baza.Plyta("Nieznany tytul", "CD");
+            plyta.WprowadzPlyte();
+            bazaPlyt.Add(plyta);
+        }
+
+        static void WypiszTytulyPlyt()
+        {
+            Console.Clear();
+            Console.WriteLine("Lista tytułów płyt:");
+            for (int i = 0; i < bazaPlyt.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {bazaPlyt[i].Tytul}");
+            }
+            Console.WriteLine("Naciśnij dowolny klawisz aby kontynuować...");
+            Console.ReadKey();
+        }
+
+        static void WyswietlSzczegolyPlyty()
+        {
+            Console.Clear();
+            Console.WriteLine("Podaj numer płyty do wyświetlenia szczegółów:");
+            int numerPlyty;
+            if (int.TryParse(Console.ReadLine(), out numerPlyty) && numerPlyty > 0 && numerPlyty <= bazaPlyt.Count)
+            {
+                var plyta = bazaPlyt[numerPlyty - 1];
+                Console.WriteLine($"Szczegóły płyty {plyta.Tytul}:");
+                plyta.WypiszPlyteSzczegoly();
+            }
+            else
+            {
+                Console.WriteLine("Niepoprawny numer płyty.");
+            }
+            Console.Write("Chcesz wyświetlić szczegóły konkretnego utworu? (T/N): ");
+            char tn = Console.ReadKey().KeyChar;
+            if (tn == 'T' || tn == 't')
+            {
+                bazaPlyt[numerPlyty - 1].WypiszUtworSzczegoly();
+            }
+
+            Console.WriteLine("\nNaciśnij dowolny klawisz aby kontynuować...");
+            Console.ReadKey();
+        }
+
+        static void ZapiszBazeDoPliku()
+        {
+            using (StreamWriter sw = new StreamWriter("baza.txt"))
+            {
+                foreach (var plyta in bazaPlyt)
+                {
+                    sw.WriteLine("Tytul: " + plyta.Tytul);
+                    sw.WriteLine("Typ: " + plyta.Typ);
+                    sw.WriteLine("Czas trwania: " + plyta.TotalCzas + " minut");
+                    sw.WriteLine("\n");
+                    foreach (var utwor in plyta.Utwory)
+                    {
+                        sw.WriteLine(utwor.Nazwa + " " + utwor.CzasTrwania + ":" + utwor.Sekundy.ToString("D2"));
+                        sw.WriteLine(string.Join(", ", utwor.Wykonawcy));
+                        sw.WriteLine(utwor.Kompozytor);
+                        sw.WriteLine("ID: " + utwor.Nr);
+                        sw.WriteLine("\n");
+                    }
+                }
+            }
+            Console.WriteLine("Baza została zapisana do pliku.");
+            Console.WriteLine("Naciśnij dowolny klawisz aby kontynuować...");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ bazaZplytamiCZESC2/Baza.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using static BazaZplytami.Baza;
+
+namespace BazaZplytami
+{
+    public class Baza
+    {
+        public struct Utwor
+        {
+            public string Nazwa;
+            public double CzasTrwania;
+            public int Sekundy;
+            public List<string> Wykonawcy;
+            public string Kompozytor;
+            public double Nr;
+
+            public Utwor(string nazwa, double czasTrwania, int sekundy, string kompozytor, double nr, List<string> wykonawcy = null)
+            {
+                Nazwa = nazwa;
+                CzasTrwania = czasTrwania;
+                Sekundy = sekundy;
+                Kompozytor = kompozytor;
+                Nr = nr;
+                Wykonawcy = wykonawcy ?? new List<string>();
+            }
+        }
+
+        public struct Plyta
+        {
+            public string Tytul;
+            public string Typ;
+            public double TotalCzas;
+            public List<Utwor> Utwory;
+            public List<string> Wszystkie;
+
+            public Plyta(string tytul, string typ)
+            {
+                Tytul = tytul;
+                Typ = typ;
+                TotalCzas = 0;
+                Utwory = new List<Utwor>();
+                Wszystkie = new List<string>();
+            }
+
+            public void WprowadzPlyte()
+            {
+                do
+                {
+                    try
+                    {
+                        Console.Write("Podaj tytuł płyty: ");
+                        Tytul = Console.ReadLine();
+
+                        Console.Write("Podaj typ płyty (CD/DVD): ");
+                        Typ = Console.ReadLine();
+                        if (Typ.ToLower() != "cd" && Typ.ToLower() != "dvd")
+                        {
+                            throw new Exception("Niepoprawny typ dysku");
+                        }
+
+                        for (int i = 0; i < 30; i++)
+                        {
+                            Console.WriteLine($"\nWprowadzanie utworu {i + 1}:");
+                            var utwor = new Utwor();
+
+                            utwor.Nr = i + 1;
+
+                            Console.Write("Podaj tytul utworu: ");
+                            utwor.Nazwa = Console.ReadLine();
+
+                            Console.Write("Podaj czas trwania utworu (w minutach): ");
+                            utwor.CzasTrwania = Convert.ToDouble(Console.ReadLine());
+
+                            Console.Write("Podaj czas trwania utworu (w sekundach): ");
+                            utwor.Sekundy = Convert.ToInt32(Console.ReadLine());
+
+                            int dodatkoweMinuty = utwor.Sekundy / 60;
+                            int pozostaleSekundy = utwor.Sekundy % 60;
+                            utwor.CzasTrwania += dodatkoweMinuty;
+                            utwor.Sekundy = pozostaleSekundy;
+
+                            TotalCzas += utwor.CzasTrwania + dodatkoweMinuty;
+
+                            Console.Write("Podaj kompozytora utworu: ");
+                            utwor.Kompozytor = Console.ReadLine();
+
+                            Console.Write("Podaj liczbę wykonawców utworu: ");
+                            int liczbaWykonawcow = Convert.ToInt32(Console.ReadLine());
+                            utwor.Wykonawcy = new List<string>();
+                            for (int j = 0; j < liczbaWykonawcow; j++)
+                            {
+                                Console.Write($"Podaj wykonawcę {j + 1}: ");
+                                string lokalna = Console.ReadLine();
+                                utwor.Wykonawcy.Add(lokalna);
+                                Wszystkie.Add(lokalna);
+                            }
+
+                            Utwory.Add(utwor);
+
+                            Console.Write("\nCzy chcesz dodać kolejny utwór? (T/N): ");
+                            string odpowiedz = Console.ReadLine();
+                            if (odpowiedz.ToLower() != "t")
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                }
+                while (true);
+            }
+
+            public void WypiszUtworSzczegoly()
+            {
+                while (true)
+                {
+                    Console.Write("\nPodaj numer utworu: ");
+                    int index = Convert.ToInt32(Console.ReadLine());
+                    index--;
+                    if (index >= 0 && index < Utwory.Count)
+                    {
+                        Utwor utwor = Utwory[index];
+                        Console.WriteLine($"\nSzczegóły utworu {index + 1}:");
+                        Console.WriteLine($"Tytul: {utwor.Nazwa}, Czas trwania: {utwor.CzasTrwania}:{utwor.Sekundy:D2}");
+                        Console.Write("Wykonawca(-y): ");
+                        foreach (var wykonawca in utwor.Wykonawcy)
+                        {
+                            Console.Write($"{wykonawca}, ");
+                        }
+                        Console.WriteLine($"\nKompozytor: {utwor.Kompozytor}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niepoprawny numer utworu.");
+                    }
+
+                    Console.Write("\nChcesz wyswietlic szczegóły kolejnego utworu? (T/N): ");
+                    char odpowiedz1 = Console.ReadKey().KeyChar;
+                    if (odpowiedz1 != 't' && odpowiedz1 != 'T')
+                    {
+                        break;
+                    }
+                }
+            }
+
+            public void WypiszPlyteSzczegoly()
+            {
+                Console.Clear();
+                Console.WriteLine($"Tytuł płyty: {Tytul}");
+                Console.WriteLine($"Typ płyty: {Typ}");
+                Console.WriteLine($"Czas trwania płyty: {TotalCzas} minut");
+                Console.WriteLine("\nLista utworów:");
+                foreach (var utwor in Utwory)
+                {
+                    Console.WriteLine($"{utwor.Nr}. {utwor.Nazwa} {utwor.CzasTrwania}:{utwor.Sekundy:D2}");
+                }
+                Console.WriteLine($"Lista wykonawców na płycie {Tytul}:");
+                foreach (var wykonawca in Wszystkie)
+                {
+                    Console.WriteLine($"- {wykonawca}");
+                }
+            }
+        }
+    }
+}
